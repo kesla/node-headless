051bdd60aab1ec679753d5de61550203464ea6b4
Spencer Alger <spencer@spenceralger.com>
2013-11-22 12:43:53 -0700

    added check for enoent error, as well as a general 'error' event handler on the child process, preventing 'uncaught error event' on systems without Xvfb installed

--- index.js
+++ index.js
@@ -29,18 +29,38 @@ module.exports = function headless(startnum, callback) {
     // assume starting Xvfb takes less than 500 ms and continue if it hasn't
     // exited during that time
     var timeout = setTimeout(function() {
-      childProcess.removeListener('exit', onExit);
+      cleanUpListeners();
       callback(null, childProcess, servernum);
     }, 500);
 
     function onExit() {
       clearTimeout(timeout);
+      cleanUpListeners();
+
       servernum++;
       headless(servernum, callback);
+    }
+
+    function onError (err) {
+      clearTimeout(timeout);
+      cleanUpListeners();
+
+      if (~err.message.indexOf('ENOENT')) {
+        callback(new Error('Xvfb is not installed or is not in your $PATH'));
+      } else {
+        callback(err);
+      }
     }
 
     // if Xvfb exits prematurely the servernum wasn't valid.
     // Happens if there's already an X-server running on @servernum but no file was created in /tmp
     childProcess.once('exit', onExit);
+    // if Xvfb is not installed, the childProcess will emit an ENOENT error
+    childProcess.once('error', onError);
+
+    function cleanUpListeners() {
+      childProcess.removeListener('exit', onExit);
+      childProcess.removeListener('error', onError);
+    }
   });
 }
